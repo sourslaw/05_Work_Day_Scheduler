c8ba58346cae14276987bc9fdab1b3b9a88861f0
sourslaw <lawanscott@gmail.com>
2021-04-20 14:34:48 -0700

    updated basic logic, updated html display

--- assets/script.js
+++ assets/script.js
@@ -1,44 +1,43 @@
+
+
+
+// form
+var formEl = $('.taskForm');
+// button
+const buttonEl = $('.btn');
+// hour of day fields
+const nineAM = $('#0900AM');
+const tenAM = $('#1000AM');
+const elevenAM = $('#1100AM');
+const noon = $('#1200AM');
+const onePM = $('#0100PM');
+const twoPM = $('#0200PM');
+const threePM = $('#0300PM');
+const fourPM = $('#0400PM');
+const fivePM = $('#0500PM');
+
+
+function handleFormSubmit(event) {
+    // Prevent the default behavior
+	event.preventDefault();
+
+	localStorage.setItem('0900AM', JSON.stringify(`${nineAM.val()}`));
+	localStorage.setItem('1000AM', JSON.stringify(`${tenAM.val()}`));
+	localStorage.setItem('1100AM', JSON.stringify(`${elevenAM.val()}`));
+	localStorage.setItem('1200PM', JSON.stringify(`${noon.val()}`));
+	localStorage.setItem('0100PM', JSON.stringify(`${onePM.val()}`));
+	localStorage.setItem('0200PM', JSON.stringify(`${twoPM.val()}`));
+	localStorage.setItem('0300PM', JSON.stringify(`${threePM.val()}`));
+	localStorage.setItem('0400PM', JSON.stringify(`${fourPM.val()}`));
+	localStorage.setItem('0500PM', JSON.stringify(`${fivePM.val()}`));
+};
+
+// Submit event on the form
+formEl.on('submit', handleFormSubmit);
+
+
+
+
 // moment
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do, YYYY  |  h:mm a"));
-
-const scheduleField = $('#scheduleField');
-
-// creating the fields
-for (let hour = 1; hour <= 9; hour++) {
-	// the row
-	let hourBlock = $('<div>');
-	hourBlock.attr('class','row')
-
-	// first column for time
-	let colEl1 = $('<div>');
-	colEl1.attr('class','col-lg-2')
-
-	let pee = $('<p>')
-	pee.text(hour)
-	let top = colEl1.append(pee)
-
-	// second column for field and save button
-	let colEl2 = $('<div>');
-	colEl2.attr('class','col-lg-10')
-
-	let inputClass = $('<div>');
-	inputClass.attr('class', 'input-group mb-3')
-
-	let inputEl = $('<input>');
-	inputEl.attr('type', 'text');
-	inputEl.attr('class', 'form-control');
-
-	let buttonEl = $('<button>');
-	buttonEl.attr('class', 'btn btn-outline-secondary');
-	buttonEl.attr('type', 'button');
-	buttonEl.attr('id', '');
-	buttonEl.text("save");
-
-	// appending to field
-	let bottom = colEl2.append(inputClass.append(inputEl, buttonEl))
-
-	hourBlock.append(top, colEl2)
-
-	scheduleField.append(hourBlock);
-};
